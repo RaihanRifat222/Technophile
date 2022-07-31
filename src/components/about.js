83810745329c876fbc22e0d7f3658c526f9f3890
RaihanRifat222 <raihanrifat222@gmail.com>
2022-07-31 14:37:16 +0600

    2nd commit

--- src/components/about.js
+++ src/components/about.js
@@ -4,26 +4,34 @@ const about = () => {
   return (
     <>
     <Container style={{marginTop: '50px'}}>
-      <h1>Dummy Text</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati tempora excepturi, fugiat dolor commodi nisi quos dolorum magni eos maiores voluptatem rem nulla et consectetur ad quis veritatis. Fuga, reiciendis.</p>
+      <h1>About Us</h1>
+        <p>Technophile is the most premium & multi-branded omnichannel retail in Bangladesh. The journey started with a single outlet in the year 2022, and now in a 10-years timeframe, there are 23 premium outlets of Technophile, all over Dhaka City. It's a renowned name to all, for their authenticity & top-notch quality. In 2021, Technophile started working as a tech e-commerce company.</p>
         <Row>
           <Col md={6}>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt aut excepturi, accusantium tempora eaque accusamus aperiam, nostrum ipsam odio quod, soluta unde aliquam molestias nihil possimus earum velit quis. Esse vel ipsam, architecto nihil ducimus ratione dignissimos porro tempore sint! Dicta eos quasi voluptates ipsum, illum modi quisquam dolor consectetur placeat dignissimos sapiente ipsa culpa repudiandae veritatis aspernatur, minima tenetur facere iure, doloremque eaque omnis debitis magni. Inventore est labore consequuntur rem hic consequatur, itaque, sequi fugiat non obcaecati totam nulla, minus odit mollitia eum ipsam. Voluptatem, necessitatibus blanditiis mollitia deserunt totam pariatur eum sunt cum quibusdam excepturi maxime vero.</p>
+            <p>Technophile is a Hybrid now, as we are operating through a fully functioning e-commerce site, as well as the other online social media platforms: Facebook, WhatsApp, Instagram & LinkedIn.</p>
           </Col>
           <Col md={6}>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam quam impedit iure ea soluta maxime quas, aliquid magnam exercitationem molestias odit cumque. Quia aliquam iusto consectetur animi non perferendis obcaecati? Ipsum nisi temporibus neque nostrum quia. Nam voluptatum fugit nihil enim soluta iste vitae. Et exercitationem suscipit ab modi. Unde eius itaque totam recusandae, tempore, obcaecati quod assumenda quisquam veritatis architecto hic veniam voluptates perspiciatis temporibus maiores delectus, facilis ratione? Voluptatem ut repellat ratione hic. Possimus eos placeat cumque consectetur, aspernatur vel officia optio fuga tenetur provident eaque inventore error suscipit laudantium praesentium adipisci quae nam ipsum? Dolores, incidunt.</p>
+            <p>It has a wide range of international premium brands gadgets & accessories collections, starting from Apple, Samsung, OnePlus, Xiaomi, Vivo, Oppo, Huawei, Skull Candy, JBL, Bose, Edifier, DJI, Amazfit, Belkin, Spigen, SwitchEasy, etc.</p>
             </Col>
 
         </Row>
         <Row>
-          <h1>Our Chef</h1>
-          <Col md={3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam esse aliquam quasi libero natus dolores cumque alias, facere quam necessitatibus rerum maxime a, perspiciatis provident non similique! Enim voluptates eius mollitia corporis doloremque tempora dolores id quam voluptatibus ab omnis quidem inventore, exercitationem, eligendi quas, est fugit iste dolorem error?
+          <h1>Our Products</h1>
+          <Col md={3}>Phone, Tablet, Speaker, True wireless, Memory card, Hard drive, Pendrive, Screen protector, Gimble, Action camera, AI security system, Charger, Adapter, Power Bank, Smart tracker - Gadget & Gear has it all.
+          </Col>
+          <Col md={3}>Technophile has turned into a trusted platform among tech-savvy people owing to different attributes, such as - a trusted online and offline shopping platform, a wide range of international brands, fast delivery service, genuine products only, and an official warranty facility, which ultimately ensures a phenomenal & premium experience.
+          </Col>
+          {/* <Col md={3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam esse aliquam quasi libero natus dolores cumque alias, facere quam necessitatibus rerum maxime a, perspiciatis provident non similique! Enim voluptates eius mollitia corporis doloremque tempora dolores id quam voluptatibus ab omnis quidem inventore, exercitationem, eligendi quas, est fugit iste dolorem error?
           </Col>
           <Col md={3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam esse aliquam quasi libero natus dolores cumque alias, facere quam necessitatibus rerum maxime a, perspiciatis provident non similique! Enim voluptates eius mollitia corporis doloremque tempora dolores id quam voluptatibus ab omnis quidem inventore, exercitationem, eligendi quas, est fugit iste dolorem error?
+          </Col> */}
+          
+        </Row>
+        <Row>
+        <h1>Our Vision</h1>
+          <Col md={3}>Along the way, Technophile has earned domestic and global recognition for its experience and proven track record in a variety of electronics fields. Walton is the pioneer of developing state of the art designs and modern technology having leading market share specializing in Multi-Stored Refrigerators, Freezers, Air Conditioners, LED/ LCD televisions, Motorcycles, Smart Phones and Home Appliances.
           </Col>
-          <Col md={3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam esse aliquam quasi libero natus dolores cumque alias, facere quam necessitatibus rerum maxime a, perspiciatis provident non similique! Enim voluptates eius mollitia corporis doloremque tempora dolores id quam voluptatibus ab omnis quidem inventore, exercitationem, eligendi quas, est fugit iste dolorem error?
-          </Col>
-          <Col md={3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam esse aliquam quasi libero natus dolores cumque alias, facere quam necessitatibus rerum maxime a, perspiciatis provident non similique! Enim voluptates eius mollitia corporis doloremque tempora dolores id quam voluptatibus ab omnis quidem inventore, exercitationem, eligendi quas, est fugit iste dolorem error?
+          <Col md={3}>Technophile is working on carrying the flag of red and green into the global market presence from the present 40 countries to more than 200 countries to dominate Go Global and the top five Electronics Brand of the Globe within 2030.
           </Col>
         </Row>
     </Container>
