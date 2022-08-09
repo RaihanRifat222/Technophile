1d06fb2e338312cb74959ef82734e658b396f188
RaihanRifat222 <raihanrifat222@gmail.com>
2022-08-09 17:28:29 +0600

    commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,28 @@
 import React, {useState} from 'react'
 import { Card,Button, Row, Col } from 'react-bootstrap'
 import products from '../productdata'
+import Modal from 'react-bootstrap/Modal';
 
 
 
 const Product = ({p}) => {
   const [varient, setVarient] = useState (p.varients[0]);
   const [quantity, setQuantity] = useState (1);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
  
   return (
-    
+    <>
     <Card style={{ width: '18rem', marginTope: '30px'}}>
-      <Card.Img variant="top" src={p.image} style={{height: '300px', width: '250px'}} />
+      <Card.Img variant="top" 
+      src={p.image} 
+      style={{height: '300px', width: '250px'}
+      
+      } 
+      onClick={handleShow}
+      />
       <Card.Body>
         <Card.Title>{p.name}</Card.Title>
         <hr/>
@@ -53,9 +64,32 @@ const Product = ({p}) => {
       </Card.Body>
     </Card>
 
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{p.name}</Modal.Title>
+        </Modal.Header>
+        
+        <Modal.Body>
+          <div>
+          <Card.Img variant="top" 
+            src={p.image} 
+            style={{height: '300px', width: '250px'}
+      
+      } 
+      />
+          </div>
+          <div>
+            <h4>Descpription</h4>
+            <h6>{p.description}</h6>
+          
+          </div>
+          </Modal.Body>
+        <Modal.Footer>
+          
+        </Modal.Footer>
+      </Modal>
 
-
-
+    </>
   )
 }
 
