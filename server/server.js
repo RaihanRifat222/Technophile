3312dade282ba843a26114559a35ba6630bbb4fd
RaihanRifat222 <raihanrifat222@gmail.com>
2022-08-14 04:10:02 +0600

    commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express= require('express')
+const dotenv = require('dotenv')
+const connectDB = require ('./config/config')
 require('colors')
 const morgan= require('morgan')
+const bgMagenta =require('colors')
+
+//config dotenv
+dotenv.config()
+
+//connection mongodb
+connectDB()
 
 const app= express()
 
@@ -10,8 +19,11 @@ app.use(morgan('dev'))
 
 //route
 app.get('/', (req, res)=>{
-    res.send('<h1>hello from node <h1/>')
+    res.send('<h1>hello from node serverrr <h1/>')
 })
-app.listen(8080, () => {
-    console.log('Server running smoothly')
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`Server running On ${process.env.NODE_ENV} mode on port no ${process.env.PORT}`.bgMagenta.white)
 })
+
